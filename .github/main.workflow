fce3c9e2e09574a1a1bfc61396b8ed3c43dd1fd1
Nicolas Coutin <Ilshidur@users.noreply.github.com>
2018-12-21 12:32:10 +0100

    Discord Action (#3)
    * feat(actions): discord notification

--- .github/main.workflow
+++ .github/main.workflow
@@ -1,10 +1,25 @@
 workflow "Notification on push" {
   on = "push"
-  resolves = ["Slack notification"]
+  resolves = [
+    "Slack notification",
+    "Discord notificaction",
+    "Discord notificaction CUSTOM",
+  ]
 }
 
 action "Slack notification" {
   uses = "Ilshidur/actions/slack@master"
   secrets = ["SLACK_WEBHOOK"]
+  args = "A new commit has been pushed to Ilshidur/actions."
+}
+
+action "Discord notificaction" {
+  uses = "Ilshidur/actions/discord@discord"
+  secrets = ["DISCORD_WEBHOOK"]
+}
+
+action "Discord notificaction CUSTOM" {
+  uses = "Ilshidur/actions/discord@discord"
+  secrets = ["DISCORD_WEBHOOK"]
   args = "A new commit has been pushed to Ilshidur/actions."
 }
--- discord/entrypoint.sh
+++ discord/entrypoint.sh
@@ -3,23 +3,23 @@
 set -eu
 
 # Check if arguments provided
-if (( $# != 1 ))
+if [ $# -eq 0 ]
 then
-
-# If argument provided, override the Discord message.
-
-echo Sending $*
-
-# TODO: Overriding
-# payload={\"username\": \"$GITHUB_ACTION\", \"text\": \"$GITHUB_REPOSITORY/$GITHUB_WORKFLOW triggered by $GITHUB_ACTOR ($GITHUB_EVENT_NAME) :\n$*\", \"icon_url\": \"https://raw.githubusercontent.com/quintessence/slack-icons/master/images/github-logo-slack-icon.png\"}
-curl -X POST --data-urlencode "" $DISCORD_WEBHOOK
-
-else
 
 # If argument NOT provided, let Discord show the event informations.
 
 echo Sending event informations
 
-curl -X POST --data-urlencode "$(cat $GITHUB_EVENT_PATH)" $DISCORD_WEBHOOK/github
+echo Payload : $(cat $GITHUB_EVENT_PATH)
+
+curl -X POST -H "Content-Type: application/json" --data "$(cat $GITHUB_EVENT_PATH)" $DISCORD_WEBHOOK/github
+
+else
+
+# If argument provided, override the Discord message.
+
+echo Sending : $*
+
+curl -X POST -H "Content-Type: application/json" --data "{ \"content\": \"$*\" }" $DISCORD_WEBHOOK
 
 fi
