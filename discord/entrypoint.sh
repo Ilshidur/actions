1f489bb4d80c6fe45c6aa32e78cd59a241d47901
Nicolas COUTIN <ncoutin@scalair.fr>
2018-12-21 12:28:38 +0100

    fix(discord): custom message action

--- discord/entrypoint.sh
+++ discord/entrypoint.sh
@@ -3,23 +3,23 @@
 set -eu
 
 # Check if arguments provided
-if (( $# != 1 ))
+if [ $# -eq 0 ]
 then
-
-# If argument provided, override the Discord message.
-
-echo Sending $*
-
-curl -X POST --data-urlencode "{ \"content\": \"$*\" }" $DISCORD_WEBHOOK
-
-else
 
 # If argument NOT provided, let Discord show the event informations.
 
 echo Sending event informations
 
-echo $(cat $GITHUB_EVENT_PATH)
+echo Payload : $(cat $GITHUB_EVENT_PATH)
 
 curl -X POST -H "Content-Type: application/json" --data "$(cat $GITHUB_EVENT_PATH)" $DISCORD_WEBHOOK/github
+
+else
+
+# If argument provided, override the Discord message.
+
+echo Sending : $*
+
+curl -X POST -H "Content-Type: application/json" --data "{ \"content\": \"$*\" }" $DISCORD_WEBHOOK
 
 fi
